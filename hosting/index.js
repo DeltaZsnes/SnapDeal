7c4f8718071968fbcb79e8ac0f99dbec88c4fb85
Daniel Zheng <daniel.zheng@barnebys.com>
2020-01-15 00:23:39 +0800

    added sign in

---
+++ hosting/index.js
@@ -0,0 +1,22 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD372hF2u5R6Lk0x6SlN6pNlwS1o6NdNQc",
+  authDomain: "snapdeal-bcafd.firebaseapp.com",
+  databaseURL: "https://snapdeal-bcafd.firebaseio.com",
+  projectId: "snapdeal-bcafd",
+  storageBucket: "snapdeal-bcafd.appspot.com",
+  messagingSenderId: "414865392784",
+  appId: "1:414865392784:web:4157813e4f819877c84415",
+  measurementId: "G-HHSB3R47NK"
+};
+
+const runAsync = async () => {
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  const provider = new firebase.auth.GoogleAuthProvider();
+  // provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+  const result = await firebase.auth().signInWithPopup(provider);
+  console.log(result);
+};
+  
+runAsync()
+.catch(console.error);
